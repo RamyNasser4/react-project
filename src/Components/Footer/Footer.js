b28d1395067f848d425405c157409dadac12f79c
RamyNasser4 <ramy_rnz@hotmail.com>
2023-09-17 02:56:57 +0300

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
     if(location.pathname.substring(0,6)!="/admin"){
         return(
             <div className="bg-[#F0F0F0] h-[25vh] flex flex-wrap justify-between items-center p-10 ">
-                <div className="font-[MaiseeMedium]">Developed by Me</div>
+                <div className="font-[MaiseeMedium]">Developed by Me &  <a href="https://salinaka-ecommerce.web.app/">Design from</a></div>
                 <div className="font-[MaiseeMedium]">Logo</div>
                 <div className="font-[MaiseeMedium]">&copy;Copyright 2023</div>
             </div>
